295876abc84916031568b6fcc7b2108971481bba
Sandip Tank <tanksandip778@gmail.com>
2019-11-13 01:53:18 +0530

    date 12-11-2019

---
+++ java/programs/VARIBLE.java
@@ -0,0 +1,12 @@
+
+
+public class VARIBLE
+{
+    public static void main(String arg[])
+    {
+        System.out.println("varible demo");
+        int n;
+        n=15;
+        System.out.println(n);
+    }
+}
---
+++ java/programs/message.java
@@ -0,0 +1,10 @@
+
+public class message {
+    public static void main(String args[])
+    {
+        System.out.println("hello world km chho");
+        System.out.println("maja ma ne badha");
+        System.out.print("hello world km chho");
+        System.out.println("maja ma ne badha");
+    }
+}
